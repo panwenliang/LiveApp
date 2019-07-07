c1fec879ac202dce6d6c09c017f3feb293e70653
panwenliang <panwenliang2008@gmail.com>
2019-07-07 21:32:28 +0900

    aaaa

---
+++ LiveApp/LiveApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LiveApp
+//
+//  Created by 刘杰 on 2019/7/7.
+//  Copyright © 2019 刘杰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
